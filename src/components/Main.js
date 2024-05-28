4fc2ccdea16864002e6ce26afaf672cf471f021d
rybolev <dark18@yandex.ru>
2024-05-28 17:02:02 +0500

    main about

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,35 @@
 import React from "react";
 import '../static/css/Main.css'
+import {Link} from "react-router-dom";
 
 export default () => {
   return (
     <main>
-      TEST
+      <div className='about-container'>
+        <div className='left'>
+          <h1>
+            сервис по поиску<br/>
+            публикаций<br/>
+            о компании<br/>
+            по его ИНН
+          </h1>
+          <p>
+            Комплексный анализ публикаций, получение данных в формате PDF на электронную почту.
+          </p>
+          <Link to='/'>Запросить данные</Link>
+        </div>
+        {/*<img src='/img/main_1.png' alt='Service'/>*/}
+      </div>
+      <div className='why-us'>
+
+      </div>
+
+      <img className='img-why-us' src='/img/main-why-us.png' alt=''/>
+
+      <div className='main-rates'>
+
+      </div>
+
     </main>
   )
 }
